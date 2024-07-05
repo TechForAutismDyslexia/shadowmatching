214a537804baeeade77afc98ecf04483bdbc549b
ELECTROMONKEY24 <yssmc24@gmail.com>
2024-07-05 16:36:23 +0530

    issuesfix

--- src/Components/Result.js
+++ src/Components/Result.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import './Result.css'
-export default function Result({tries,timer}) {
+import React, { useEffect } from 'react';
+import './Result.css';
+import axios from 'axios';
+
+export default function Result({ tries, timer }) {
+  useEffect(() => {
+    async function sendData() {
+      try {
+        if (tries === 0|| timer === 0) {
+          return;
+        }
+        const response = await axios.post('https://jwlgamesbackend.vercel.app/api/caretaker/sendgamedata', {
+          gameId: 4,
+          tries: tries,
+          timer: timer,
+          status: true,
+        });
+        console.log(response);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    sendData();
+  }, []);
+
   return (
     <div className='main container mt-5 flex justify-content-center align-items-center'>
-        <h1>Result Page</h1>
-        <p>The number of tries : {tries}</p>
-        <p>The time taken : {(timer/1000)} seconds</p>
+      <h1>Result Page</h1>
+      <p>The number of tries : {tries}</p>
+      <p>The time taken : {(timer / 1000)} seconds</p>
     </div>
-  )
+  );
 }
